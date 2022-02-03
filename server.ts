3d460407d7563d9a65475a33296eec78addcf545
eliudheredia <edu.heredia@ymail.com>
2022-02-02 22:13:05 -0500

    HW1

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express, {NextFunction, Request, Response} from 'express';
+import express, {Request, Response} from 'express';
 import UserController from './controllers/UserController';
 import TuitController from './controllers/TuitController';
 import UserDao from './daos/UserDao';
@@ -6,7 +6,6 @@ import TuitDao from './daos/TuitDao';
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 const app = express();
-const router = app._router();
 
 mongoose.connect('mongodb://localhost:27017/tuiter')
 app.use(bodyParser.json())
@@ -17,30 +16,10 @@ app.get('/hello', (req: Request, res: Response) =>
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
 
-// UserController.getInstance(app);
-// TuitController.getInstance(app);
-
 const userDao = new UserDao();
 const tuitDao = new TuitDao();
 const userController = new UserController(app, userDao);
 const tuitController = new TuitController(app, tuitDao);
-
-// app.get('/users', (req: Request, res: Response) =>
-// res.send(userController));
-// app.get('/tuits', (req: Request, res: Response) =>
-//     res.send(tuitController));
-
-// app.use('/users', userController);
-// app.use('/tuits', tuitController);
-
-router.route('/users').get(userController.app.bind(userController));
-// app.route('/tuits')
-//     .get((req: Request, res: Response) =>
-//         res.send(tuitController))
-
-// router.use((req: Request, res: Response, next: NextFunction) =>
-// userController);
-// router.get('/users')
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
