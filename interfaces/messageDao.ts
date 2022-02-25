d2cb2ca11ce00bdaa6e5d9eaa6bee1a4e466d72d
eliud26 <58715824+eliud26@users.noreply.github.com>
2022-02-25 18:35:45 -0500

    Update messageDao.ts

--- interfaces/messageDao.ts
+++ interfaces/messageDao.ts
@@ -7,7 +7,7 @@ export default interface MessageDao {
     createMessage(uid: string, suid: string, message: Message): Promise<Message>;
     findMessagesByUser(uid: string): Promise<Message[]>;
     findMessagesToUser(tid: string): Promise<Message[]>;
-    deleteMessage(uid: string, ruid: string): Promise<any>;
+    deleteMessage(uid: string, ruid: string, mid: string): Promise<any>;
     updateMessage(uid: string, ruid: string, message: Message): Promise<any>;
     findAllMessages(): Promise<Message[]>;
 }
